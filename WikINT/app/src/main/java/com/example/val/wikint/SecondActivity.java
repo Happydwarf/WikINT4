0d77828320dc50eee22a420faff1eba1595c5a2a
LAVRIC\Val <valentin.lavric13@gmail.com>
2017-12-12 12:48:03 +0100

    login

---
+++ WikINT/app/src/main/java/com/example/val/wikint/SecondActivity.java
@@ -0,0 +1,70 @@
+package com.example.val.wikint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+/**
+ * Created by Val on 12/12/2017.
+ */
+
+public class SecondActivity extends Activity {
+
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("Accueil");
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText("User Profil");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button button = (Button) findViewById(R.id.association);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), AssociationList.class);
+                startActivity(i);
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.events);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), EventList.class);
+                startActivity(i);
+            }
+        });
+    }
+}

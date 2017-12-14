52f57e829c68969b835947c748a5bdd827ca45cb
Alexis Blervaque <alexis.blervaque@telecom-sudparis.eu>
2017-12-14 15:39:05 +0100

    Version debut firebase

--- WikINT/app/src/main/java/com/example/val/wikint/AssociationDetail.java
+++ WikINT/app/src/main/java/com/example/val/wikint/AssociationDetail.java
@@ -1,19 +1,11 @@
 package com.example.val.wikint;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
-
-import java.util.List;
 
 /**
  * Created by alexisblervaque on 24/11/2017.
@@ -22,49 +14,14 @@ import java.util.List;
 public class AssociationDetail extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.association_detail_layout);
 
-        // Recovery of data from Association list
-
-        Bundle bundle = getIntent().getBundleExtra("Association");
-        Association asso = bundle.getParcelable("Asso");
+        TextView description = (TextView)findViewById(R.id.description);
 
 
-        // Automaticaly create the template of the association asso
+        description.setText(getIntent().getStringExtra("description"));
 
-        String name = asso.getName();
-        TextView nom_asso = (TextView)findViewById(R.id.nom_asso);
-        nom_asso.setText(name);
-
-        String description = asso.getDescription();
-        TextView descriptionView = (TextView)findViewById(R.id.description);
-        descriptionView.setText(description);
-
-        String president = asso.getPresident();
-        TextView pres = (TextView)findViewById(R.id.nom_prez);
-        pres.setText(president);
-
-        String local = asso.getLocal();
-        TextView emplacement_local = (TextView)findViewById(R.id.lieu_local);
-        emplacement_local.setText(local);
-
-        String pp = asso.getPictures().get(0);
-        ImageView photo_profil = (ImageView) findViewById(R.id.profil_picture);
-        int imageId = getResources().getIdentifier(pp,"drawable",getPackageName());
-        photo_profil.setImageResource(imageId);
-
-        String cover = asso.getPictures().get(1);
-        ImageView cover_pict = (ImageView) findViewById(R.id.cover_picture);
-        int imageId2 = getResources().getIdentifier(cover,"drawable",getPackageName());
-        cover_pict.setImageResource(imageId2);
-
-
-        // Attach the different buttons to the different activities
-
-        Button button2 = (Button) findViewById(R.id.button_event);
-
-        Button button = (Button) findViewById(R.id.button_photos);
     }
 }
--- WikINT/app/src/main/java/com/example/val/wikint/AssociationList.java
+++ WikINT/app/src/main/java/com/example/val/wikint/AssociationList.java
@@ -1,32 +1,12 @@
 package com.example.val.wikint;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.GridLayout;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by alexisblervaque on 24/11/2017.
@@ -34,126 +14,22 @@ import java.util.Date;
 
 public class AssociationList extends Activity {
 
-    private GridLayout AssoContainer;
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.association_list);
 
 
-        AssoContainer = findViewById(R.id.AssoContainer);
-
-        DecodeJson JsonData = new DecodeJson(this);
-        ArrayList<Association> associations = JsonData.getAssociations();
-
-
-
-
-
-        AssoContainer.setRowCount(associations.size()/2 + associations.size()%2);
-
-        int i = 0;
-        for (Association asso : associations)
-        {
-            GridLayout.Spec titleTxtSpecColumn = GridLayout.spec(i%2,GridLayout.FILL);
-            GridLayout.Spec titleRowSpec = GridLayout.spec(i/2);
-            FrameLayout frame = createFrame(asso);
-            AssoContainer.addView(frame, new GridLayout.LayoutParams(titleRowSpec , titleTxtSpecColumn) );
-            i++;
-        }
-
-
-
-    }
-
-    private FrameLayout createFrame(final Association asso)
-    {
-        FrameLayout result = new FrameLayout(this);
-        result.setLayoutParams(new FrameLayout.LayoutParams(AssoContainer.getWidth()/2,120));
-
-        ImageButton button = new ImageButton(this);
-        button.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        button.setBackgroundColor(Color.TRANSPARENT);
-        button.setPadding(5,5,5,5);
-        button.setImageResource(R.drawable.button);
-
+        Button button = (Button)findViewById(R.id.detail);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(v.getContext(),AssociationDetail.class);
-                Bundle b = new Bundle();
-                b.putParcelable("Asso",asso);
-                i.putExtra("Association", b);
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), AssociationDetail.class);
+                String idOfButton = ((Button)(view)).getTag().toString();
+                i.putExtra("idOfButton",idOfButton);
+                i.putExtra("description","Voivi la description faisons un test");
                 startActivity(i);
-
             }
         });
-
-        ImageView image = new ImageView(this);
-        LinearLayout.LayoutParams imageParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        imageParam.gravity = Gravity.CENTER;
-        image.setLayoutParams(imageParam);
-        int imageId = getResources().getIdentifier(asso.getPictures().get(0),"drawable",getPackageName());
-        image.setImageResource(imageId);
-        image.setPadding(10,30,10,10);
-
-
-        TextView text = new TextView(this);
-        LinearLayout.LayoutParams textParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
-        textParam.gravity = Gravity.BOTTOM;
-        text.setLayoutParams(textParam);
-        text.setPadding(10,10,10,10);
-        text.setText(asso.getName());
-        text.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
-
-
-        result.addView(button);
-        result.addView(image);
-        result.addView(text);
-
-
-        return result;
-    }
-
-    private class JsonData
-    {
-        private ArrayList<Association> associations;
-        private ArrayList<Event> events;
-
-        public ArrayList<Association> getAssociations() {
-            return associations;
-        }
-
-        public void setAssociations(ArrayList<Association> associations) {
-            this.associations = associations;
-        }
-
-        public ArrayList<Event> getEvents() {
-            return events;
-        }
-
-        public void setEvents(ArrayList<Event> events) {
-            this.events = events;
-        }
-    }
-
-
-    public String loadJSONFromAsset(Context context) {
-        String json = null;
-        try {
-            InputStream is = context.getAssets().open("Associations.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 }
